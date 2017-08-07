621eb6609995263c17eaaf18e4151269b3348859
Your Name <you@example.com>
2017-08-07 16:47:20 +0530

    Added sysout statements

--- src/main/java/biz/dss/dao/Product.java
+++ src/main/java/biz/dss/dao/Product.java
@@ -2,28 +2,30 @@ package biz.dss.dao;
 
 /*@Component*/
 public class Product {
-	private int id;
-	private String name;
-	private double price;
-	public int getId() {
-		return id;
-	}
-	public void setId(int id) {
-		this.id = id;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public double getPrice() {
-		return price;
-	}
-	public void setPrice(double price) {
-		this.price = price;
-	}
-	
-	System.out.println("Hello World");
+    private int id;
+    private String name;
+    private double price;
+    public int getId() {
+        System.out.println("Hello World");
+        return this.id;
+    }
+    public void setId(final int id) {
+        System.out.println("Hello World");
+        this.id = id;
+    }
+    public String getName() {
+        return this.name;
+    }
+    public void setName(final String name) {
+        this.name = name;
+    }
+    public double getPrice() {
+        return this.price;
+    }
+    public void setPrice(final double price) {
+        this.price = price;
+    }
+
+
 
 }
