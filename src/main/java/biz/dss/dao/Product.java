fb5bfffb0cd7ba488aabb85516efacb5045b8865
Hemlata Kalokhe <you@example.com>
2017-08-08 12:02:50 +0530

    Added

--- src/main/java/biz/dss/dao/Product.java
+++ src/main/java/biz/dss/dao/Product.java
@@ -27,6 +27,8 @@ public class Product {
         return this.price;
     }
     public void setPrice(final double price) {
+        System.out.println("Hello World");
+        System.out.println("Hello World");
         this.price = price;
     }
 }
