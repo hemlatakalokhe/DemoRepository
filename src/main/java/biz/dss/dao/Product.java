8286037a5b76b5d3bc26727aa6f564bf2d3ccb9f
Your Name <you@example.com>
2017-08-08 10:01:02 +0530

    Product.java

--- src/main/java/biz/dss/dao/Product.java
+++ src/main/java/biz/dss/dao/Product.java
@@ -20,9 +20,11 @@ public class Product {
         return this.name;
     }
     public void setName(final String name) {
+        System.out.println("Hello World");
         this.name = name;
     }
     public double getPrice() {
+        System.out.println("Hello World");
         return this.price;
     }
     public void setPrice(final double price) {
