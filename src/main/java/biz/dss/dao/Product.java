bd300720b78f1520ea8d46bb1fbbc860f14a892f
Hemlata Kalokhe <you@example.com>
2017-08-08 11:48:37 +0530

    Added

--- src/main/java/biz/dss/dao/Product.java
+++ src/main/java/biz/dss/dao/Product.java
@@ -15,9 +15,13 @@ public class Product {
     }
     public String getName() {
         System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
         return this.name;
     }
     public void setName(final String name) {
+        System.out.println("Hello World");
+        System.out.println("Hello World");
         System.out.println("Hello World");
         System.out.println("Hello World");
         System.out.println("Hello World");
