29ed02d0428290ff61bb6e06657479bf0e4165eb
Hemlata Kalokhe <you@example.com>
2017-08-08 14:47:15 +0530

    Revert "Revert "Removed Statements""
    This reverts commit 1bbbf66c9575e9ce4d652ebe4d6bc1b22bf7f2f4.

--- src/main/java/biz/dss/dao/Product.java
+++ src/main/java/biz/dss/dao/Product.java
@@ -10,22 +10,15 @@ public class Product {
     }
 
     public void setId(final int id) {
-        System.out.println("Hello World");
-        System.out.println("Hello World");
-        System.out.println("Hello World");
         this.id = id;
     }
     public String getName() {
-        System.out.println("Hello World");
-        System.out.println("Hello World");
         return this.name;
     }
     public void setName(final String name) {
-        System.out.println("Hello World");
         this.name = name;
     }
     public double getPrice() {
-        System.out.println("Hello World");
         return this.price;
     }
     public void setPrice(final double price) {
